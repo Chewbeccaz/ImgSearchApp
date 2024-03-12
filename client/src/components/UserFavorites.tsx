4630ec98c8be5ec982db92ee4c32584574800e21
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-03-12 21:13:04 +0100

    fixed printing favorites and some styling

--- client/src/components/UserFavorites.tsx
+++ client/src/components/UserFavorites.tsx
@@ -41,37 +41,44 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+import { Image } from "../models/Image";
+import "../styles/userfavorites.css";
 
 export const UserFavorites = () => {
   const { user } = useAuth0();
-  const [favoriteImages, setFavoriteImages] = useState([]);
+  const [favoriteImages, setFavoriteImages] = useState<string[]>([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
+    if (!user || !user.email) return;
+
+    const userId = user.email;
+    const url = `http://localhost:3000/users/${userId}/favorites`;
+
     const fetchUserFavorites = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/user", {
-          params: {
-            userId: user?.email,
-          },
-        });
-        console.log("Response data:", response.data);
-        console.log(response);
+        const response = await axios.get(url);
         setFavoriteImages(response.data);
+        console.log("sätter Response data:", response.data);
       } catch (error) {
         console.error("fel på hämtning av favoriter1", error);
       }
     };
 
     fetchUserFavorites();
-  }, [user]);
+  }, [user?.email]);
 
   return (
     <>
-      <h2>Mina favoritbilder</h2>
-      <div>
+      <h2>Dina favoritbilder</h2>
+      <div className="favorite-grid">
         {favoriteImages.length > 0 ? (
           favoriteImages.map((image, index) => (
-            <img key={index} src={image} alt={`Favorite Image ${index}`} />
+            <div key={index} className="favorite-container">
+              <img src={image} alt={`Favorite Image ${index}`} />
+            </div>
           ))
         ) : (
           <p>Inga favoritbilder sparade</p>
