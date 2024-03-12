b309657bf1a999f8da114333da8378c060284d38
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-03-12 10:28:45 +0100

    commented out while working on a fav-button instea

---
+++ client/src/Router.tsx
@@ -0,0 +1,26 @@
+// import React from "react";
+// import { createBrowserRouter } from "react-router-dom";
+// import { Layout } from "./components/Layout";
+// import { ImgSearchApp } from "./components/ImgSearchApp";
+// import { UserFavorites } from "./components/UserFavorites";
+// import { NotFound } from "./components/NotFound";
+
+// export const Router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     errorElement: <NotFound />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <ImgSearchApp />,
+//         errorElement: <NotFound />,
+//       },
+//       {
+//         path: "/userfavorites",
+//         element: <UserFavorites />,
+//         errorElement: <NotFound />,
+//       },
+//     ],
+//   },
+// ]);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+// import { RouterProvider } from "react-router-dom";
+// import { Router } from "../src/Router.tsx";
+
+// import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}>
+      {/* <RouterProvider router={Router}> */}
       <App />
+      {/* </RouterProvider> */}
     </Auth0Provider>
   </React.StrictMode>
 );
