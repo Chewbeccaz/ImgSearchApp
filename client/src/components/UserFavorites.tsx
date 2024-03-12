e3d2c008e64771187f393bb9ea098265958fe1a0
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-03-12 14:40:38 +0100

    added comments on what to do

--- client/src/components/UserFavorites.tsx
+++ client/src/components/UserFavorites.tsx
@@ -38,6 +38,45 @@
 //   );
 // };
 
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+
 export const UserFavorites = () => {
-  return <div>Favoritbilderna</div>;
+  const { user } = useAuth0();
+  const [favoriteImages, setFavoriteImages] = useState([]);
+
+  useEffect(() => {
+    const fetchUserFavorites = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/user", {
+          params: {
+            userId: user?.email,
+          },
+        });
+        console.log("Response data:", response.data);
+        console.log(response);
+        setFavoriteImages(response.data);
+      } catch (error) {
+        console.error("fel på hämtning av favoriter1", error);
+      }
+    };
+
+    fetchUserFavorites();
+  }, [user]);
+
+  return (
+    <>
+      <h2>Mina favoritbilder</h2>
+      <div>
+        {favoriteImages.length > 0 ? (
+          favoriteImages.map((image, index) => (
+            <img key={index} src={image} alt={`Favorite Image ${index}`} />
+          ))
+        ) : (
+          <p>Inga favoritbilder sparade</p>
+        )}
+      </div>
+    </>
+  );
 };
