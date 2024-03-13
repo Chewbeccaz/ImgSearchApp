1e8dc9a841c4aadbaa684bb199ac26c2d161a161
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-03-13 11:53:29 +0100

    added searchtime<p>

--- client/src/components/SearchResult.tsx
+++ client/src/components/SearchResult.tsx
@@ -1,66 +1,3 @@
-// import { useState } from "react";
-// import "../styles/SearchResults.css";
-// import FavoriteButton from "./FavoriteButton";
-// import { Image } from "../models/Image";
-// import axios from "axios";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// interface SearchResultsProps {
-//   images: Image[];
-// }
-
-// export const SearchResult: React.FC<{ images: Image[] }> = ({ images }) => {
-//   console.log("vilka bilder skickas med?", images);
-//   const [favoriteImage, setFavoriteImage] = useState("");
-//   const { user } = useAuth0();
-
-//   const handleFavoriteClick = async (imgUrl: string) => {
-//     setFavoriteImage(imgUrl);
-
-//     // Logic for saving favorite image...
-//     try {
-//       const response = await axios.post(
-//         "http://localhost:3000/test",
-//         {
-//           id: user?.email,
-//           favoriteImage: imgUrl,
-//         },
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//         }
-//       );
-//       console.log("Användaren är sparad", response.data);
-//     } catch (error) {
-//       console.log("Användaren sparades inte", error);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div className="search-results">
-//         <h2>Sökresultat</h2>
-//         <div className="image-grid">
-//           {images.map((result, index) => (
-//             <div className="image-container" key={index}>
-//               <img
-//                 src={result.link}
-//                 alt={`Bild ${result.title}`}
-//                 className="image"
-//               />
-//               <FavoriteButton
-//                 onClick={() => handleFavoriteClick(result.link)}
-//                 imgUrl={result.link}
-//               />
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
 import { useState } from "react";
 import "../styles/SearchResults.css";
 import FavoriteButton from "./FavoriteButton";
@@ -68,21 +5,24 @@ import { Image } from "../models/Image";
 
 interface SearchResultsProps {
   images: Image[];
+  searchTime: string;
 }
 
-export const SearchResult: React.FC<{ images: Image[] }> = ({ images }) => {
+export const SearchResult: React.FC<SearchResultsProps> = ({
+  images,
+  searchTime,
+}) => {
   const [favoriteImage, setFavoriteImage] = useState("");
 
   const handleFavoriteClick = (imgUrl: string) => {
     setFavoriteImage(imgUrl);
-
-    // Logic for saving favorite image...
   };
 
   return (
     <>
       <div className="search-results">
         <h2>Sökresultat</h2>
+        {searchTime && <p>Sökningen tog {searchTime} sekunder.</p>}
         <div className="image-grid">
           {images.map((result, index) => (
             <div className="image-container" key={index}>
